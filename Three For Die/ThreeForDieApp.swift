789ebcf9b71d7d1efd0907d8a98e549b1038ab41
tommy-adams <thomasadams012@gmail.com>
2023-02-18 14:25:23 -0800

    more init

---
+++ Three For Die/ThreeForDieApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct Three_For_DieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HStack {
+                Text ("Hello, World!")
+            }
+        }
+    }
+}
