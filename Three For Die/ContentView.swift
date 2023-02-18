eb8bee90d98ee8ca7887b41cf5577ec302accf70
tommy-adams <thomasadams012@gmail.com>
2023-02-18 14:24:02 -0800

    init

---
+++ Three For Die/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Three For Die
+//
+//  Created by Thomas Adams on 2/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Three For Die/Three_For_DieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_For_DieApp.swift
+//  Three For Die
+//
+//  Created by Thomas Adams on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Three_For_DieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
