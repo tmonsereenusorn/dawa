99d3cda6c31dc2b9cd3f1c7987564de5dbf1f261
Jonathan Coronado <jcoronad@stanford.edu>
2023-02-18 18:37:54 -0800

    created event and location structs, implemented fetchParticipants, createEvent, and addEvent

--- Three For Die/Models/User.swift
+++ Three For Die/Models/User.swift
@@ -17,3 +17,39 @@ struct User {
         self.bio = bio
     }
 }
+
+struct Location {
+    var latitude: Double
+    var longitude: Double
+
+    init (latitude: Double = 0, longitude: Double = 0) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
+
+struct Event {
+    var name: String
+    var numPeopleReq: Int
+    var numPeopleCur: Int
+    var description: String
+    var host: String
+    var participants: [String]
+    var location: Location // need to make location struct
+    var time: Date
+    var activity: String
+    
+    init (name: String = "", numPeopleReq: Int = 0, numPeopleCur: Int = 0, description: String = "", 
+            host: String = "", participants: [String] = [], location: Location = "", time: Date = Date.now, 
+            activity: String = "") {
+        self.name = name
+        self.numPeopleReq = numPeopleReq
+        self.numPeopleCur = numPeopleCur
+        self.description = description
+        self.host = host
+        self.participants = participants
+        self.location = location
+        self.time = time
+        self.activity = activity
+    }
+}
