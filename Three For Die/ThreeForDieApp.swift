2689195003d67f1e8c4e7fc233d13dce2c5e6f23
tommy-adams <thomasadams012@gmail.com>
2023-02-18 16:44:36 -0800

    tabview

--- Three For Die/ThreeForDieApp.swift
+++ Three For Die/ThreeForDieApp.swift
@@ -1,12 +1,23 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Three_For_DieApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
-            HStack {
-                Text ("Hello, World!")
-            }
+            TabView ()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
